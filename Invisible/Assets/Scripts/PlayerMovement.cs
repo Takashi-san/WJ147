56613dc6d35f1baece1be78436140aaa63809cf2
bruno takashi <bt.tengan@gmail.com>
2020-05-06 04:23:39 -0300

    Animator da Garota

--- Invisible/Assets/Scripts/PlayerMovement.cs
+++ Invisible/Assets/Scripts/PlayerMovement.cs
@@ -7,15 +7,18 @@ using System;
 public class PlayerMovement : MonoBehaviour {
 	[SerializeField] PlayerData _playerData = null;
 	[SerializeField] TransitionData _transitionData = null;
+	Animator _animator;
 	float _speed;
 	Rigidbody2D _rb;
 	Controls _controls;
 	Vector2 _movement;
+	Vector2 _lastMovement = Vector2.zero;
 	bool _forcedMovement = false;
 
 	void Awake() {
 		_speed = _playerData.speed;
 		_rb = GetComponent<Rigidbody2D>();
+		_animator = GetComponent<Animator>();
 		_controls = new Controls();
 		_controls.Player.Enable();
 
@@ -54,13 +57,38 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate() {
 		Vector2 target;
 
+		// Animator: 0 = right, 1 = left, 2 = down, 3 = up.
 		if (_movement.x != 0) {
+			if (_movement.x > 0) {
+				_animator.SetInteger("Direction", 0);
+			}
+			else {
+				_animator.SetInteger("Direction", 1);
+			}
+			_animator.SetBool("Moving", true);
+
 			target = (Vector2)transform.position + Vector2.right * (_movement.x * _speed * Time.fixedDeltaTime);
 			_rb.MovePosition(target);
 		}
 		else if (_movement.y != 0) {
+			if (_movement.y > 0) {
+				_animator.SetInteger("Direction", 3);
+			}
+			else {
+				_animator.SetInteger("Direction", 2);
+			}
+			_animator.SetBool("Moving", true);
+
 			target = (Vector2)transform.position + Vector2.up * (_movement.y * _speed * Time.fixedDeltaTime);
 			_rb.MovePosition(target);
+		}
+		else {
+			_animator.SetBool("Moving", false);
+		}
+
+		if (_movement != _lastMovement) {
+			_animator.SetTrigger("Update");
+			_lastMovement = _movement;
 		}
 	}
 
