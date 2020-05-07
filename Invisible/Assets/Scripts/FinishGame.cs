f116118dabd023acfb905f122d68bb4539b4fd7c
bruno takashi <bt.tengan@gmail.com>
2020-05-07 04:58:56 -0300

    Game sem som

---
+++ Invisible/Assets/Scripts/FinishGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishGame : MonoBehaviour {
+	[SerializeField] GameObject _transition = null;
+	[SerializeField] string _scene = null;
+
+	void Start() {
+		FindObjectOfType<PlayerMovement>().LockMovement();
+		FindObjectOfType<SceneHandler>().LoadScene(_scene, _transition);
+	}
+}
